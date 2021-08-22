35f192a85c26008c1b9e8a936fb3f79cdf0299c3
moe-ped <m.berning@web.de>
2021-08-22 17:00:34 +0200

    Test values

--- src/Egg.cpp
+++ src/Egg.cpp
@@ -1,11 +1,17 @@
 #include <Arduino.h>
 #include <DynamixelShield.h>
+#include <sstream>
+#include <string>  
 
 const float DXL_PROTOCOL_VERSION = 2.0;
 
 DynamixelShield dxl;
 
+int t;
+
 void initializeMotor(int id);
+void wobble();
+void printStuff();
 
 void setup() {
     Serial.begin(57600);
@@ -18,20 +24,16 @@ void setup() {
     initializeMotor(1);
     initializeMotor(2);
     initializeMotor(3);
-    
-    Serial.println("hello world");
+
+    // wobble();
 }
 
 void loop() {
-    Serial.println("hello world!!!1");
-    dxl.setGoalPosition(1, 512);
-    // dxl.setGoalPosition(2, 512);
-    // dxl.setGoalPosition(3, 512);
-    delay(1000);
-    dxl.setGoalPosition(1, 0);
-    // dxl.setGoalPosition(2, 0);
-    // dxl.setGoalPosition(3, 0);
-    delay(1000);
+    printStuff();
+    dxl.setGoalPWM(1, 200);
+    t += 100;
+
+    delay(100);
 }
 
 void initializeMotor(int id) {
@@ -40,6 +42,23 @@ void initializeMotor(int id) {
 
     // Turn off torque when configuring items in EEPROM area
     dxl.torqueOff(id);
-    dxl.setOperatingMode(id, OP_POSITION);
+    dxl.setOperatingMode(id, OP_PWM);
     dxl.torqueOn(id);
+}
+
+void printStuff() {
+    float i = dxl.getPresentCurrent(1);
+    float phi = dxl.getPresentPosition(1);
+    float pwm = dxl.getPresentPWM(1);
+    float phidot = dxl.getPresentVelocity(1);
+
+    // TODO: string format
+    Serial.print("i: ");
+    Serial.print(i);
+    Serial.print("\tphi: ");
+    Serial.print(phi);
+    Serial.print("\tpwm: ");
+    Serial.print(pwm);
+    Serial.print("\tphidot: ");
+    Serial.println(phidot);
 }
